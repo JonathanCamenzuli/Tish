a7cf50977138fe9fdfb20e558899aed5b3d804a3
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2022-06-16 10:38:53 +0200

    Added more functionality for str functions

---
+++ src/terminal_io.c
@@ -0,0 +1,21 @@
+#include "terminal_io.h"
+#include <stdlib.h>
+
+string_t* init_str()
+{
+    string_t *str = (string_t*)malloc(sizeof(string_t));
+    str->string = (char*)malloc(sizeof(char));
+    str->size = 1;
+    str->string[0] = '\0';
+    return str;
+}
+
+arg_t* init_arg()
+{
+    string_t *str = (string_t*)malloc(sizeof(string_t));
+    str->string = (char*)malloc(sizeof(char));
+    str->size = 1;
+    str->string[0] = '\0';
+    return str;
+}
+
--- src/terminal_io.h
+++ src/terminal_io.h
@@ -30,4 +30,18 @@ typedef struct
     int size;       /**< the size of string itself */ 
 }arg_t;
 
+/**
+ * @brief Initialise the string composite data type
+ * 
+ * @return string_t initialised, memory allocated string
+ */
+string_t* init_str();
+
+/**
+ * @brief Initialise the argument composite data type
+ * 
+ * @return arg_t initialised, memory allocated argument
+ */
+arg_t* init_arg();
+
 #endif
