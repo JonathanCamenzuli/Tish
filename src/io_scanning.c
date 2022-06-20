4bb2531497297a288bb0977d397572614153ce8c
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2022-06-20 19:02:39 +0200

    Added basic functionality for input and validation for I/O and Advanced Scanning (#2)

--- src/io_scanning.c
+++ src/io_scanning.c
@@ -22,20 +22,16 @@ int validateTishInput(char* inputBuffer, execVars_t* execVars)
     for (int i = 0; i < strlen(inputBuffer); i++)
     {
         // First and last argument cannot be metacharacters
-        if (i == 0
-            || i == strlen(inputBuffer)- 2 
-            && inputBuffer[i] == GT_CHAR
-            && inputBuffer[i+1] == GT_CHAR)
+        if ((i == 0 || i == strlen(inputBuffer) - 2) 
+            && (inputBuffer[i] == GT_CHAR)
+            && (inputBuffer[i+1] == GT_CHAR))
         {
             fprintf(stderr, "-tish: syntax error near unexpected token \'%c%c\'\n", GT_CHAR, GT_CHAR);
             return EXIT_FAILURE;
         }
 
         if ((i == 0 || i == strlen(inputBuffer) - 1)
-            && (inputBuffer[i+2] == VBAR_CHAR
-                || inputBuffer[i+2] == GT_CHAR
-                || inputBuffer[i+2] == LT_CHAR
-                || (i == 0 && inputBuffer[i] == SCOLON_CHAR)))
+            && (inputBuffer[i+2] == VBAR_CHAR || inputBuffer[i+2] == GT_CHAR || inputBuffer[i+2] == LT_CHAR || (i == 0 && inputBuffer[i] == SCOLON_CHAR)))
         {
             fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", inputBuffer[i]);
             return EXIT_FAILURE;
@@ -43,13 +39,10 @@ int validateTishInput(char* inputBuffer, execVars_t* execVars)
         
         // Disallow adjacent metacharacters
         if (!(execVars->metacharQuote)
-            && i < strlen(inputBuffer) - 2
+            && (i < strlen(inputBuffer) - 2)
             && inputBuffer[i] == GT_CHAR
-            && inputBuffer[i + 1] == GT_CHAR
-            && (inputBuffer[i + 2] == VBAR_CHAR
-                || inputBuffer[i + 2] == GT_CHAR
-                || inputBuffer[i + 2] == LT_CHAR
-                || inputBuffer[i + 2] == SCOLON_CHAR))
+            && inputBuffer[i+1] == GT_CHAR
+            && (inputBuffer[i+2] == VBAR_CHAR || inputBuffer[i + 2] == GT_CHAR || inputBuffer[i + 2] == LT_CHAR || inputBuffer[i + 2] == SCOLON_CHAR))
         {
             fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", inputBuffer[i]);
             return EXIT_FAILURE;
@@ -58,22 +51,99 @@ int validateTishInput(char* inputBuffer, execVars_t* execVars)
         if (!(execVars->metacharQuote)
             && !(inputBuffer[i] == GT_CHAR)
             && inputBuffer[i+1] == GT_CHAR
-            && (inputBuffer[i] == VBAR_CHAR
-                || inputBuffer[i] == GT_CHAR
-                || inputBuffer[i] == LT_CHAR
-                || inputBuffer[i] == SCOLON_CHAR)
-            && (inputBuffer[i+1] == VBAR_CHAR
-                || inputBuffer[i+1] == GT_CHAR
-                || inputBuffer[i+1] == LT_CHAR
-                || inputBuffer[i+1] == SCOLON_CHAR))
+            && (inputBuffer[i] == VBAR_CHAR || inputBuffer[i] == GT_CHAR || inputBuffer[i] == LT_CHAR || inputBuffer[i] == SCOLON_CHAR)
+            && (inputBuffer[i+1] == VBAR_CHAR || inputBuffer[i+1] == GT_CHAR || inputBuffer[i+1] == LT_CHAR || inputBuffer[i+1] == SCOLON_CHAR))
         {
             fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", inputBuffer[i]);
             return EXIT_FAILURE;
         }
 
-        //Check if Metachar
-        // Pipe
-        if (!(execVars->metacharQuote) && inputBuffer[i] == VBAR_CHAR)
+        // Checking for 'Append output redirection' (>>) metacharacters
+        if(!execVars->metacharQuote && inputBuffer[i] == GT_CHAR && inputBuffer[i+1] == GT_CHAR)
+        {
+            if (!(execVars->charFound))
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c%c\'\n", GT_CHAR, GT_CHAR);
+                return EXIT_FAILURE;
+            }
+            if (execVars->outRedir)
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c%c\'\n", GT_CHAR, GT_CHAR);
+                return EXIT_FAILURE;                
+            }
+
+            *execVars = (execVars_t)
+            {
+                .metacharPipe = false,
+                .metacharOutRedir = true,
+                .outRedir = true,
+                .appendOut = true,
+                .outRedirWait = true
+            };
+
+            execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
+            execVars->pipeline[execVars->command_i][execVars->arg_i+1] = NULL;
+
+            execVars->char_i++;
+            continue;
+        }
+
+        // Checking for 'Output redirection' (>) metacharacter
+        else if (!(execVars->metacharQuote) && inputBuffer[i] == GT_CHAR)
+        {
+            if (!(execVars->charFound))
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", GT_CHAR);
+                return EXIT_FAILURE;                  
+            }
+            if (execVars->outRedir)
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", inputBuffer[i]);
+                return EXIT_FAILURE;    
+            }
+
+            *execVars = (execVars_t)
+            {
+                .metacharPipe = false,
+                .metacharOutRedir = true,
+                .outRedir = true,
+                .outRedirWait = true,
+            };
+
+            execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
+            execVars->pipeline[execVars->command_i][execVars->arg_i+1] = NULL;
+            continue;
+        }
+        // Checking for 'Input Redirection' (<) metacharacter
+        else if (!(execVars->metacharQuote) && inputBuffer[i] == LT_CHAR)
+        {
+            if (!(execVars->charFound))
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", LT_CHAR);
+                return EXIT_FAILURE; 
+            }
+            if (execVars->inRedir)
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%c\'\n", inputBuffer[i]);
+                return EXIT_FAILURE;    
+            }
+
+            *execVars = (execVars_t)
+            {
+                .inRedir = true,
+                .metacharPipe = false,
+                .metacharOutRedir = false,
+                .metacharInRedir = true,
+                .inRedirWait = true,
+            };
+
+            execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
+            execVars->pipeline[execVars->command_i][execVars->arg_i+1] = NULL;
+
+            continue;
+        }
+        // Checking for 'Pipe operator' (|) metacharacter
+        else if (!(execVars->metacharQuote) && inputBuffer[i] == VBAR_CHAR)
         {
             if (!(execVars->charFound))
             {
@@ -117,17 +187,19 @@ int validateTishInput(char* inputBuffer, execVars_t* execVars)
                     }
                 }
 
-                execVars->metacharPipe = true;
-                execVars->metacharOutRedir = false;
-                execVars->metacharInRedir = false;
+                *execVars = (execVars_t)
+                {
+                    .metacharPipe = true,
+                    .metacharOutRedir = false,
+                    .metacharInRedir = false,
+                    .arg_i = 0,
+                    .char_i = 0
+                };
 
                 execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
                 execVars->pipeline[execVars->command_i][execVars->arg_i + 1] = NULL;
 
                 execVars->command_i++;
-                execVars->arg_i = 0;
-                execVars->char_i = 0;
-
                 execVars->commandPipeCount++;
 
                 continue;
@@ -135,35 +207,207 @@ int validateTishInput(char* inputBuffer, execVars_t* execVars)
             else
                 continue;
         }
-
-        //Append Out
-        else if(!execVars->metacharQuote && inputBuffer[i] == GT_CHAR && inputBuffer[i+1] == GT_CHAR)
+        // Checking for 'New argument in input' ( ) metacharacter
+        else if (!(execVars->metacharQuote) && inputBuffer[i] == SPACE_CHAR)
         {
-            if (!(execVars->charFound))
+            if ((execVars->arg_i + 1) >= MAX_TISH_ARGS)
             {
-                fprintf(stderr, "-tish: syntax error near unexpected token \'%c%c\'\n", GT_CHAR, GT_CHAR);
-                return EXIT_FAILURE;
+                fprintf(stderr, "-tish: %s: too many arguments passed\n", execVars->pipeline[i - (execVars->commandPipeCount-1)][0]);
+                execVars->waitForSColon = true;
+                execVars->exec = false;
+                continue;
             }
-            if (execVars->outRedir)
+            if (execVars->metacharOutRedir)
             {
-                fprintf(stderr, "-tish: syntax error near unexpected token \'%c%c\'\n", GT_CHAR, GT_CHAR);
-                return EXIT_FAILURE;                
+                if (execVars->outRedirWait)
+                    continue;
+                else
+                {
+                    execVars->fileOut[execVars->out_i] = '\0';
+                    execVars->metacharOutRedir = false;
+                    execVars->outRedirWait = false;
+                    continue;
+                }
             }
+            if (execVars->metacharInRedir)
+            {
+                if (execVars->inRedirWait)
+                    continue;
+                else
+                {
+                    execVars->fileIn[execVars->in_i] = '\0';
+                    execVars->metacharInRedir = false;
+                    execVars->inRedirWait = false;
+                    continue;
+                }
+            }
+            if (i == strlen(inputBuffer) - 1)
+                return EXIT_SUCCESS;
+            if ((i == 0 || i == strlen(inputBuffer)-1)
+                || (execVars->arg_i == 0 && execVars->char_i == 0)
+                || inputBuffer[i+1] == SPACE_CHAR
+                || inputBuffer[i+1] == VBAR_CHAR
+                || inputBuffer[i+1] == GT_CHAR
+                || inputBuffer[i+1] == LT_CHAR
+                || inputBuffer[i+1] == SCOLON_CHAR
+                || inputBuffer[i-1] == VBAR_CHAR
+                || inputBuffer[i-1] == GT_CHAR
+                || inputBuffer[i-1] == LT_CHAR
+                || inputBuffer[i-1] == SCOLON_CHAR)
+                continue;
 
-            execVars->metacharPipe = false;
-            execVars->metacharOutRedir = true;
-            execVars->outRedir = true;
-            execVars->appendOut = true;
-            execVars->outRedirWait = true;
             execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
-            execVars->pipeline[execVars->command_i][execVars->arg_i + 1] = NULL;
 
-            execVars->char_i++;
+            execVars->arg_i++;
+            execVars->char_i = 0;
+            continue;
+        }
+        
+        // Checking for 'Quotation marks' (") as a metacharacter
+        else if (inputBuffer[i] == QMARK_CHAR)
+        {
+            if (execVars->metacharQuote)
+            {
+                execVars->metacharQuote = false;
+                continue;
+            }
+            execVars->metacharQuote = true;
             continue;
         }
 
-        //Output Redirect
-        else if (!(execVars->metacharQuote) && inputBuf)
+        // Strip meaning of metacharacter (\)
+        else if(inputBuffer[i] == BSLASH_CHAR)
+        {
+            if (execVars->char_i + 1 >= MAX_TISH_TOKEN_LEN)
+            {
+                fprintf(stderr, "-tish: too many characters passed to argument [%c%c%c%c...] in pipeline %d\n",
+                    execVars->pipeline[execVars->command_i][execVars->arg_i][0], execVars->pipeline[execVars->command_i][execVars->arg_i][1],
+                    execVars->pipeline[execVars->command_i][execVars->arg_i][2], execVars->pipeline[execVars->command_i][execVars->arg_i][3],
+                    execVars->pipeline[execVars->command_i][execVars->arg_i][4], execVars->command_i + 1);
+                execVars->waitForSColon = true;
+                execVars->exec = false;
+                continue;
+            }
+            execVars->pipeline[execVars->command_i][execVars->arg_i][(execVars->char_i)++] = inputBuffer[i+1];
+            i++;
+            continue;
+        }
+
+        // Checking for 'separation of multiple command pipelines' (;) metacharacter
+        else if (!(execVars->metacharQuote) && inputBuffer[i] == SCOLON_CHAR)
+        {
+            if (!(execVars->charFound))
+            {
+                fprintf(stderr, "-tish: syntax error near unexpected token \'%s\'\n", SCOLON_CHAR);
+                return EXIT_FAILURE;
+            }
+            execVars->waitForSColon = false;
+            if ((execVars->command_i + 1) >= MAX_TISH_COMMANDS)
+            {
+                fprintf(stderr, "-tish: too many pipelines passed to pipeline sequences\n");
+                return EXIT_FAILURE; 
+            }
+            if (i == strlen(inputBuffer) - 1)
+                continue;
+            
+            execVars->pipeline[execVars->command_i][execVars->arg_i][execVars->char_i] = '\0';
+            execVars->pipeline[execVars->command_i][execVars->arg_i+1] = NULL;
+
+            if (execVars->outRedir)
+                execVars->fileOut[strlen(execVars->fileOut)] = '\0';
+            else
+                execVars->fileOut = NULL;
+
+            if (execVars->inRedir)
+                execVars->fileIn[strlen(execVars->fileIn)] = '\0';
+            else
+                execVars->fileIn = NULL;
+
+            char* fileInParam;
+            char* fileOutParam;
+
+            if (execVars->inRedir)
+                fileInParam = execVars->fileIn;
+            else
+                fileInParam = NULL;
+
+            if (execVars->outRedir)
+                fileOutParam = execVars->fileOut;
+            else
+                fileOutParam = NULL;
+
+            if ( ((execVars->commandPipeCount) == 1) && execTishCommand(**(execVars->pipeline + execVars->command_i), *(execVars->pipeline + execVars->command_i)) == 0)
+            {
+                free(execVars->fileOut);
+                execVars->fileOut = malloc(MAX_FILENAME_LEN*sizeof(char));
+
+                free(execVars->fileIn);
+                execVars->fileIn = malloc(MAX_FILENAME_LEN*sizeof(char));
+
+
+                *execVars = (execVars_t)
+                {
+                    .outRedir = false,
+                    .inRedir = false,
+                    .appendOut = false,
+                    .pipe = false,
+                    .metacharPipe = false,
+                    .metacharOutRedir = false,
+                    .metacharInRedir = false,
+                    .metacharQuote = false,
+                    .arg_i = 0,
+                    .char_i = 0,
+                    .commandPipeCount = 1,
+                    .out_i = 0,
+                    .in_i = 0,
+                    .pipe = true,
+                    .outRedirWait = false,
+                    .inRedirWait = false
+                };
+
+                execVars->command_i++;
+
+                continue;
+            }
+            else if (forkExecPipe(execVars->pipeline + (execVars->command_i - (execVars->commandPipeCount - 1)), fileInParam, fileOutParam, execVars->appendOut, false) == 1)
+            {
+                fprintf(stderr, "-tish: execution failed\n");
+                return EXIT_FAILURE;
+            }
+            else
+            {
+                free(execVars->fileOut);
+                execVars->fileOut = malloc(MAX_FILENAME_LEN*sizeof(char));
+
+                free(execVars->fileIn);
+                execVars->fileIn = malloc(MAX_FILENAME_LEN*sizeof(char));
+
+                *execVars = (execVars_t)
+                {
+                    .outRedir = false,
+                    .inRedir = false,
+                    .appendOut = false,
+                    .pipe = true,
+                    .metacharPipe = false,
+                    .metacharOutRedir = false,
+                    .metacharInRedir = false,
+                    .metacharQuote = false,
+                    .arg_i = 0,
+                    .char_i = 0,
+                    .commandPipeCount = 1,
+                    .out_i = 0,
+                    .in_i = 0,
+                    .pipe = true,
+                    .outRedirWait = false,
+                    .inRedirWait = false
+                };
+
+                execVars->command_i++;
+
+                continue;
+            }
+            continue;
+        }
     }
     
     
