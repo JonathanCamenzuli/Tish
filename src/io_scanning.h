acf187845cf90ab95cbb6e497adb3e90e9a34afb
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2022-06-21 18:18:47 +0200

    Reverted to earlier implementation due to seg. fault (#2)

--- src/io_scanning.h
+++ src/io_scanning.h
@@ -7,87 +7,70 @@
  * 
  */
 
-#ifndef IO_SCANNING_H
-#define IO_SCANNING_H
-
-#include <limits.h>
-#include <stdbool.h>
-
-#define MAX_TISH_INPUT      512
-#define MAX_TISH_PIPES      16
-#define MAX_TISH_COMMANDS   16
-#define MAX_TISH_ARGS       16
-#define MAX_TISH_TOKEN_LEN  512
-#define MAX_FILENAME_LEN    128
-
-#define GT_CHAR             '>'
-#define LT_CHAR             '<'
-#define VBAR_CHAR           '|'
-#define SPACE_CHAR          ' '
-#define QMARK_CHAR          '\"'
-#define BSLASH_CHAR         '\\'
-#define SCOLON_CHAR         ';'      
+#ifndef __IO_SCANNING_H
+#define __IO_SCANNING_H
 
 /**
- * @brief Struct that contains all the variables used in the io_scanning src files
+ * @brief A struct that represents a string
+ * 
+ */
+typedef struct 
+{
+    char *string;   /**< the string itself */
+    int size;       /**< the size of string itself */
+}string_t;
+
+/**
+ * @brief A struct that represents an argument
  * 
  */
 typedef struct
 {
-    char*** pipeline;       /**< Contains the pipeline itself*/
-    char* inputBuffer;      /**< A buffer which stores the input made from the user */
-    int commandPipeCount;   /**< Keep track of command in the pipeline */
-    int command_i;          /**< Index for commands */
-    int arg_i;              /**< Index for arguments */
-    int char_i;             /**< Index for characters */
-    bool metacharPipe;      /**< Has a metacharacter for a pipe been found? */
-    bool metacharOutRedir;  /**< Has a metacharacter for output redirection been found? */
-    bool metacharInRedir;   /**< Has a metacharacter for input redirection been found? */
-    bool metacharQuote;     /**< Have quotation marks been detected? ("...") */
-    bool outRedir;          /**< Flag for output redirection */
-    bool inRedir;           /**< Flag for input redirection */
-    bool appendOut;         /**< Flag for append */
-    bool pipe;              /**< Flag for creating a pipe */
-    char* fileOut;          /**< File path for output redirection */
-    char* fileIn;           /**< File path for input redirection */
-    int out_i;              /**< Index for fileOut */
-    int in_i;               /**< Index for fileIn */
-
-    bool outRedirWait;      /**< */
-    bool inRedirWait;       /**< */
-
-    bool charFound;         /**< Flag which indicates that a character has been found */
-
-    bool waitForSColon;     /**< */
-
-    bool exec;               /**< Flag for execution */
-
-}execVars_t;
-
+    char **args;    /**< the string itself */
+    int size;       /**< the size of string itself */ 
+}arg_t;
 
 /**
- * @brief Process terminal input into a buffer
+ * @brief Initialise the string composite data type
  * 
- * @param inputBuffer   Buffer in which input is to stored
- * @return int          0 if successful, 1 if failure
+ * @return string_t initialised, memory allocated string
  */
-int getTishInput(char* inputBuffer);
+string_t* init_str();
 
 /**
- * @brief Validates the input made by the user
+ * @brief Get the length of string "object"
  * 
- * @param inputBuffer   A buffer which stores the input made from the user
- * @param index_i       Index of ongoing loop
- * @param execVars      An instance of the execVars_t struct  
- * @return int          If 0, successful, if 1, failure, if 2, continue statement 
+ * @param str String to get length from
+ * @return int length of string
  */
-int validateTishInput(char* inputBuffer, int index_i, execVars_t* execVars);
+int get_str_length(string_t* str);
 
 /**
- * @brief Process and execute user input
+ * @brief Output the string "object"
  * 
- * @return int  If 0, successful, if 1, failure
+ * @param str String to output
  */
-int processInputExec();
+void print_str(string_t* str);
+
+/**
+ * @brief Free memory allocated to string
+ * 
+ * @param str String to free memory allocated to it
+ */
+void free_str(string_t* str);
+
+/**
+ * @brief Initialise the argument composite data type
+ * 
+ * @return arg_t initialised, memory allocated argument
+ */
+arg_t* init_arg();
+
+/**
+ * @brief Output the argument "object"
+ * 
+ * @param arg Argument to output
+ */
+void print_arg(arg_t* arg);
 
 #endif
